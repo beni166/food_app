0a1381e96c631af4de88b395c538af2c7ba416f2
digitalvisiontg-ux <digitalvisiontg@gmail.com>
2025-11-02 13:25:55 +0000

    modification

--- app/Models/Promotion.php
+++ app/Models/Promotion.php
@@ -3,20 +3,28 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use App\Models\Products;  
-use App\Models\Category; 
+use App\Models\Products;
+use App\Models\Category;
+
 class Promotion extends Model
 {
-    protected $fillable = ['titre', 'products_id', 'category_id', 'reduction', 'date_debut', 'date_fin'];
+    protected $fillable = [
+        'titre',
+        'products_id',
+        'category_id',
+        'reduction',
+        'date_debut',
+        'date_fin'
+    ];
 
     public function product()
     {
-        return $this->belongsTo(products::class);
+        return $this->belongsTo(Products::class, 'products_id');
     }
 
     public function category()
     {
-        return $this->belongsTo(category::class);
+        return $this->belongsTo(Category::class, 'category_id');
     }
 
     public function isActive()
